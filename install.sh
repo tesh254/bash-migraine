69a8c2b20694a32fa0b905746a5bf9d3a02b5c95
tesh254 <ewachira254@gmail.com>
2023-09-29 02:03:09 +0300

    format bash script

--- install.sh
+++ install.sh
@@ -1,56 +1,63 @@
+Copy code
 #!/bin/bash
 
 CLI_NAME="migraine"
 REPO_OWNER="tesh254"
 REPO_NAME="migraine"
 
-BOLD=\e[1m
-RESET=\e[0m
-
+BOLD="\033[1m"
+RESET="\033[0m"
+GREEN="\033[32m"
+RED="\033[31m"
+BLUE="\033[34m"
+CHECK="✓"
+CROSS="˟"
 
 if command -v jq &> /dev/null
 then
-    echo "jq is installed"
+    echo "${BOLD}${GREEN}:::bash::: jq is installed ${CHECK}${RESET}"
+    # Get OS and convert to lowercase
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+    
+    # Determine CPU architecture
+    ARCH=$(uname -m)
+    
+    # Get release version without spaces
+    VERSION=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | jq -r '.tag_name' | tr -d '[:space:]')
+    RELEASE_VERSION="${VERSION:1}"
+    echo "${BOLD}${BLUE}:::bash::: ${CLI_NAME} latest version $RELEASE_VERSION${RESET}"
+    
+    # Get release URL
+    RELEASE_URL="https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/v$RELEASE_VERSION/${CLI_NAME}_${RELEASE_VERSION}_${OS}_${ARCH}.tar.gz"
+    echo "${BOLD}${GREEN}:::bash::: binary archive URL: $RELEASE_URL ${CHECK}${RESET}"
+    
+    echo "${BOLD}${BLUE}:::bash::: creating a temporary directory for downloading and extracting $CLI_NAME${RESET}"
+    TMP_DIR=$(mktemp -d)
+    
+    echo "${BOLD}${BLUE}:::bash::: starting download...${RESET}"
+    curl -L -o "$TMP_DIR/$CLI_NAME-$RELEASE_VERSION.tar.gz" "$RELEASE_URL"
+    
+    echo "${BOLD}${BLUE}:::bash::: extraction started...${RESET}"
+    tar -xzf "$TMP_DIR/$CLI_NAME-$RELEASE_VERSION.tar.gz" -C "$TMP_DIR"
+    
+    echo "${BOLD}${BLUE}:::bash::: moving binary to a system bin directory"
+    if [ "$OS" == "darwin" ] || [ "$OS" == "linux" ]; then
+        INSTALL_DIR="/usr/local/bin"
+        sudo mv "$TMP_DIR/$CLI_NAME" "$INSTALL_DIR/$CLI_NAME"
+        echo "${BOLD}${BLUE}:::bash::: making migraine executable...${RESET}"
+        sudo chmod +x "$INSTALL_DIR/$CLI_NAME"
+        echo "${BOLD}${GREEN}:::bash::: making file executable ${CHECK}${RESET}"
+    else
+        echo "${BOLD}${RED}:::bash::: $OS operating system is not supported  ${RESET}"
+        exit 1
+    fi
+    
+    echo "${BOLD}${BLUE}:::bash::: cleaning up the temporary directory...${RESET}"
+    rm -rf "$TMP_DIR"
+    
+    echo "${BOLD}${GREEN}:::bash::: $CLI_NAME installed successfully $RELEASE_VERSION s${CHECK}${RESET}"
 else
-    echo "jq is not installed"
+    echo "${BOLD}${RED}:::bash::: jq is not installed;${RESET} please refer to ${BOLD}${GREEN}https://github.com/tesh254/migraine#readme${RESET} on how to install it"
     exit 1
 fi
 
-# Get OS and convert to lowercase
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-
-# Determine CPU architecture
-ARCH=$(uname -m)
-
-# Get release version without spaces
-VERSION=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | jq -r '.tag_name' | tr -d '[:space:]')
-RELEASE_VERSION="${VERSION:1}"
-echo ":::bash::: latest version $RELEASE_VERSION"
-
-# Get release URL
-RELEASE_URL="https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/v$RELEASE_VERSION/${CLI_NAME}_${RELEASE_VERSION}_${OS}_${ARCH}.tar.gz"
-echo ":::bash::: binary archive URL: $RELEASE_URL"
-
-echo ":::bash::: creating a temporary directory for downloading and extracting $CLI_NAME"
-TMP_DIR=$(mktemp -d)
-
-echo ":::bash::: starting download..."
-curl -L -o "$TMP_DIR/$CLI_NAME-$RELEASE_VERSION.tar.gz" "$RELEASE_URL"
-
-echo ":::bash::: extraction started..."
-tar -xzf "$TMP_DIR/$CLI_NAME-$RELEASE_VERSION.tar.gz" -C "$TMP_DIR"
-
-echo ":::bash::: moving binary to a system bin directory"
-if [ "$OS" == "darwin" ] || [ "$OS" == "linux" ]; then
-    INSTALL_DIR="/usr/local/bin"
-    sudo mv "$TMP_DIR/$CLI_NAME" "$INSTALL_DIR/$CLI_NAME"
-    sudo chmod +x "$INSTALL_DIR/$CLI_NAME"
-else
-    echo ":::bash::: $OS operating system is not supported"
-    exit 1
-fi
-
-echo ":::bash::: cleaning up the temporary directory"
-rm -rf "$TMP_DIR"
-
-echo ":::bash::: $CLI_NAME installed successfully $RELEASE_VERSION"
