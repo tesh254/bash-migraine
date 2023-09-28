d584b1cd0c7ba320272aabac2456634ca1218ccb
tesh254 <ewachira254@gmail.com>
2023-09-29 02:18:53 +0300

    fix color unicodes not showing correctly

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-Copy code
 #!/bin/bash
 
 CLI_NAME="migraine"
@@ -15,7 +14,7 @@ CROSS="˟"
 
 if command -v jq &> /dev/null
 then
-    echo "${BOLD}${GREEN}:::bash::: jq is installed ${CHECK}${RESET}"
+    echo -e "${BOLD}${GREEN}:::bash::: jq is installed ${CHECK}${RESET}"
     # Get OS and convert to lowercase
     OS=$(uname -s | tr '[:upper:]' '[:lower:]')
     
@@ -25,39 +24,39 @@ then
     # Get release version without spaces
     VERSION=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | jq -r '.tag_name' | tr -d '[:space:]')
     RELEASE_VERSION="${VERSION:1}"
-    echo "${BOLD}${BLUE}:::bash::: ${CLI_NAME} latest version $RELEASE_VERSION${RESET}"
+    echo -e "${BOLD}${BLUE}:::bash::: ${CLI_NAME} latest version $RELEASE_VERSION${RESET}"
     
     # Get release URL
     RELEASE_URL="https://github.com/$REPO_OWNER/$REPO_NAME/releases/download/v$RELEASE_VERSION/${CLI_NAME}_${RELEASE_VERSION}_${OS}_${ARCH}.tar.gz"
-    echo "${BOLD}${GREEN}:::bash::: binary archive URL: $RELEASE_URL ${CHECK}${RESET}"
+    echo -e "${BOLD}${GREEN}:::bash::: binary archive URL: $RELEASE_URL ${CHECK}${RESET}"
     
-    echo "${BOLD}${BLUE}:::bash::: creating a temporary directory for downloading and extracting $CLI_NAME${RESET}"
+    echo -e "${BOLD}${BLUE}:::bash::: creating a temporary directory for downloading and extracting $CLI_NAME${RESET}"
     TMP_DIR=$(mktemp -d)
     
-    echo "${BOLD}${BLUE}:::bash::: starting download...${RESET}"
+    echo -e "${BOLD}${BLUE}:::bash::: starting download...${RESET}"
     curl -L -o "$TMP_DIR/$CLI_NAME-$RELEASE_VERSION.tar.gz" "$RELEASE_URL"
     
-    echo "${BOLD}${BLUE}:::bash::: extraction started...${RESET}"
+    echo -e "${BOLD}${BLUE}:::bash::: extraction started...${RESET}"
     tar -xzf "$TMP_DIR/$CLI_NAME-$RELEASE_VERSION.tar.gz" -C "$TMP_DIR"
     
-    echo "${BOLD}${BLUE}:::bash::: moving binary to a system bin directory"
+    echo -e "${BOLD}${BLUE}:::bash::: moving binary to a system bin directory"
     if [ "$OS" == "darwin" ] || [ "$OS" == "linux" ]; then
         INSTALL_DIR="/usr/local/bin"
         sudo mv "$TMP_DIR/$CLI_NAME" "$INSTALL_DIR/$CLI_NAME"
-        echo "${BOLD}${BLUE}:::bash::: making migraine executable...${RESET}"
+        echo -e "${BOLD}${BLUE}:::bash::: making migraine executable...${RESET}"
         sudo chmod +x "$INSTALL_DIR/$CLI_NAME"
-        echo "${BOLD}${GREEN}:::bash::: making file executable ${CHECK}${RESET}"
+        echo -e "${BOLD}${GREEN}:::bash::: making file executable ${CHECK}${RESET}"
     else
-        echo "${BOLD}${RED}:::bash::: $OS operating system is not supported  ${RESET}"
+        echo -e "${BOLD}${RED}:::bash::: $OS operating system is not supported  ${RESET}"
         exit 1
     fi
     
-    echo "${BOLD}${BLUE}:::bash::: cleaning up the temporary directory...${RESET}"
+    echo -e "${BOLD}${BLUE}:::bash::: cleaning up the temporary directory...${RESET}"
     rm -rf "$TMP_DIR"
     
-    echo "${BOLD}${GREEN}:::bash::: $CLI_NAME installed successfully $RELEASE_VERSION s${CHECK}${RESET}"
+    echo -e "${BOLD}${GREEN}:::bash::: $CLI_NAME installed successfully $RELEASE_VERSION ${CHECK}${RESET}"
 else
-    echo "${BOLD}${RED}:::bash::: jq is not installed;${RESET} please refer to ${BOLD}${GREEN}https://github.com/tesh254/migraine#readme${RESET} on how to install it"
+    echo -e "${BOLD}${RED}:::bash::: jq is not installed;${RESET} please refer to ${BOLD}${GREEN}https://github.com/tesh254/migraine#readme${RESET} on how to install it"
     exit 1
 fi
 
